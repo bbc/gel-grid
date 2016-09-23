a429af2e5bc5788af84b30900024713fe4e0a290
Matthew Crouch <matt@crazypanda.co.uk>
2016-09-23 13:56:57 +0100

    Added deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+git clone https://$GH_TOKEN@github.com/bbc/gel-grid.css.git
+git config user.name "Travis CI"
+git config user.email $EMAIL
+
+npm run dist
+mv dist/gel-grid.css gel-grid.css/gel-grid.css
+mv dist/gel-grid.min.css gel-grid.css/gel-grid.min.css
+
+cd gel-grid.css
+git add -A .
+git commit -m "Built dist from bbc/gel-grid $TRAVIS_COMMIT"
+git push -q upstream HEAD:master
