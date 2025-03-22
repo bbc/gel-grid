908108741e8eb6a1f3b529e5ff6475eb74dc6abc
Sean Carroll <sean.carroll@bbc.co.uk>
2025-03-22 21:58:17 +0000

    use loadpaths when running tests

--- test/setup-jest.js
+++ test/setup-jest.js
@@ -1,7 +1,7 @@
 const sass = require('sass');
 
 global.compileCSS = async (filePath = '') => {
-  const { css } = await sass.compileAsync(filePath);
+  const { css } = await sass.compileAsync(filePath, { loadPaths: ['./node_modules'] });
 
   return css;
 };
