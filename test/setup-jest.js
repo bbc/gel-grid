db4e46c0ff1238a05fe07185d9cc2a27b67b64ed
Sean Carroll <sean.carroll@bbc.co.uk>
2025-10-01 08:44:33 +0100

    remove jest setup script

--- test/setup-jest.js
+++
@@ -1,7 +0,0 @@
-const sass = require('sass');
-
-global.compileCSS = async (filePath = '') => {
-  const { css } = await sass.compileAsync(filePath, { loadPaths: ['./node_modules'] });
-
-  return css;
-};
