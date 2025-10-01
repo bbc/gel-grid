716f3cf24f0fc1c2a54ee21b3e6085e1d3ecf5cc
Sean Carroll <sean.carroll@bbc.co.uk>
2025-10-01 07:42:35 +0100

    chore: Avoid pre and post scripts (#86)
    ## Description
    Using `npm config set ignore-scripts true` means that any scripts with
    `pre...` and `post...` will no longer run. Whilst this may improve the
    security around npm scripts it does mean that the workflows need to be
    adjusted - which is what this PR does.
    
    At the same time the package.json prettier command has been updated to
    ensure more files are formatted consistently.

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -10,40 +10,37 @@ var autoprefixerOptions = { browsers: supportedBrowsers };
 
 // cssnano config
 var nanoOptions = {
-    autoprefixer: { browsers: supportedBrowsers },
-    convertValues: { length: false, boolean: false },
-    mergeRules: false
+  autoprefixer: { browsers: supportedBrowsers },
+  convertValues: { length: false, boolean: false },
+  mergeRules: false,
 };
 
 // compile the Sass
-gulp.task('sass', function() {
-    return gulp.src('src/assets/sass/*.scss')
-        .pipe(
-            plugins.sass()
-                .on('error', plugins.sass.logError)
-        )
-        .pipe(plugins.autoprefixer(autoprefixerOptions))
-        .pipe(plugins.cssnano(nanoOptions))
-        .pipe(gulp.dest('src/assets/css'));
+gulp.task('sass', function () {
+  return gulp
+    .src('src/assets/sass/*.scss')
+    .pipe(plugins.sass().on('error', plugins.sass.logError))
+    .pipe(plugins.autoprefixer(autoprefixerOptions))
+    .pipe(plugins.cssnano(nanoOptions))
+    .pipe(gulp.dest('src/assets/css'));
 });
 
 // watch files for changes
-gulp.task('watch', function() {
-    gulp.watch('src/assets/sass/**/*.scss', ['sass']);
-    gulp.watch(['src/index.html', 'src/assets/images/**/*'], ['copy']);
+gulp.task('watch', function () {
+  gulp.watch('src/assets/sass/**/*.scss', ['sass']);
+  gulp.watch(['src/index.html', 'src/assets/images/**/*'], ['copy']);
 });
 
 gulp.task('copy', function () {
-    gulp.src(['!./src/assets/sass/**/*', '!./src/bower_components/**/*', './src/**/*'])
-        .pipe(gulp.dest('./dist/'));
+  gulp
+    .src(['!./src/assets/sass/**/*', '!./src/bower_components/**/*', './src/**/*'])
+    .pipe(gulp.dest('./dist/'));
 });
 
 // default task
 gulp.task('default', ['sass', 'copy', 'watch']);
 
 // deploy task
-gulp.task('deploy', ['sass', 'copy'], function() {
-    return gulp.src('./dist/**/*')
-        .pipe(plugins.ghPages());
-        
+gulp.task('deploy', ['sass', 'copy'], function () {
+  return gulp.src('./dist/**/*').pipe(plugins.ghPages());
 });
